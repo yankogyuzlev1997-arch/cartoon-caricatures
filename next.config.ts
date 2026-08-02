7f556b4476ab7546f96404b19c218cd25e550dcc
yankogyuzlev1997-arch <yankogyuzlev1997@gmail.com>
2026-08-02 19:00:07 +0300

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   poweredByHeader: false,
-  distDir: 'work/next-cache-disabled',
   images: { formats: ['image/avif','image/webp'] },
 };
 export default nextConfig;
